50de9444db6e9dd04caddab35d20d91736295da5
Ngoc Tuan Truong <truongtuan97@yahoo.com>
2026-04-08 14:27:57 +0700

    ready to deploy pro

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,31 @@ import express from 'express';
 import taskRoute from './routes/task.route.js';
 import { connectDb } from './config/db.js';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 
 // middlewares
 const app = express();
 const PORT = Number(process.env.PORT) || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json());
-app.use(cors({origin: ["http://localhost:5173",]}))
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({ origin: ["http://localhost:5173",] })) 
+}
+
 app.use('/api/tasks', taskRoute);
+
+if (process.env.NODE_ENV === 'production') {
+
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 const startServer = async () => {
     await connectDb();
